b9dedf78d8a9bc2ee1ca6959b25cb01220345a39
Blandine <blandine.mallart@gmail.com>
2019-12-18 23:35:44 +0100

    Création des composants header footer citationCards

---
+++ frontend/santathon/src/app/citation-cards/citation-cards.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-citation-cards',
+  templateUrl: './citation-cards.component.html',
+  styleUrls: ['./citation-cards.component.scss']
+})
+export class CitationCardsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
