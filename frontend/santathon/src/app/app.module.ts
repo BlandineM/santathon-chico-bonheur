b8248f9f645b7bf81c7e9c36616fa2d38801504e
Philippe Lambet <philippe.lambert.fr@gmail.com>
2019-12-19 14:18:00 +0100

    yo

--- frontend/santathon/src/app/app.module.ts
+++ frontend/santathon/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { CitationCardsComponent } from './citation-cards/citation-cards.component';
+import { PodiumComponent } from './podium/podium.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    CitationCardsComponent
+    CitationCardsComponent,
+    PodiumComponent
   ],
   imports: [
     BrowserModule,
